007f951c92e5ec4a26ef9aae02c7f4a3702ea598
PedroHenriqueFerreira <HPedro09062004@gmail.com>
2023-01-20 20:22:54 -0300

    mapped types

--- src/user/dto/login-user.dto.ts
+++ src/user/dto/login-user.dto.ts
@@ -1,14 +1,7 @@
-import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
+import { PickType } from '@nestjs/mapped-types';
+import { RegisterUserDto } from './register-user.dto';
 
-export class LoginUserDto {
-  @IsEmail({}, { message: 'Este email é inválido' })
-  @Length(3, 64, { message: 'O email deve ter entre 3 e 64 caracteres' })
-  @IsString({ message: 'O email precisa ser uma string' })
-  @IsNotEmpty({ message: 'O email é um campo obrigatório' })
-  email: string;
-
-  @Length(8, 64, { message: 'O nome deve ter entre 3 e 64 caracteres' })
-  @IsString({ message: 'A senha precisa ser uma string' })
-  @IsNotEmpty({ message: 'O email é um campo obrigatório' })
-  password: string;
-}
+export class LoginUserDto extends PickType(RegisterUserDto, [
+  'email',
+  'password',
+] as const) {}
